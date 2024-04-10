95d1fa175e7f7f0205f95764752eab394b702143
marielsgtzz <marielsgtzz@gmail.com>
2024-04-10 16:16:40 -0600

    Cada estado tiene script de Estado

--- EcosistemasMexicanos/Assets/Scripts/GameControllerMapaEcosistemas.cs
+++ EcosistemasMexicanos/Assets/Scripts/GameControllerMapaEcosistemas.cs
@@ -1,10 +1,49 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
 
 public class GameControllerMapaEcosistemas : MonoBehaviour
 {
+
+    [Header("Estados")]
+    [SerializeField] private GameObject Aguascalientes;
+    [SerializeField] private GameObject BajaCalifornia;
+    [SerializeField] private GameObject BajaCaliforniaSur;
+    [SerializeField] private GameObject Campeche;
+    [SerializeField] private GameObject Chiapas;
+    [SerializeField] private GameObject Chihuahua;
+    [SerializeField] private GameObject CiudadDeMexico;
+    [SerializeField] private GameObject Coahuila;
+    [SerializeField] private GameObject Colima;
+    [SerializeField] private GameObject Durango;
+    [SerializeField] private GameObject EstadoDeMexico;
+    [SerializeField] private GameObject Guanajuato;
+    [SerializeField] private GameObject Guerrero;
+    [SerializeField] private GameObject Hidalgo;
+    [SerializeField] private GameObject Jalisco;
+    [SerializeField] private GameObject Michoacan;
+    [SerializeField] private GameObject Morelos;
+    [SerializeField] private GameObject Nayarit;
+    [SerializeField] private GameObject NuevoLeon;
+    [SerializeField] private GameObject Oaxaca;
+    [SerializeField] private GameObject Puebla;
+    [SerializeField] private GameObject Queretaro;
+    [SerializeField] private GameObject QuintanaRoo;
+    [SerializeField] private GameObject SanLuisPotosi;
+    [SerializeField] private GameObject Sinaloa;
+    [SerializeField] private GameObject Sonora;
+    [SerializeField] private GameObject Tabasco;
+    [SerializeField] private GameObject Tamaulipas;
+    [SerializeField] private GameObject Tlaxcala;
+    [SerializeField] private GameObject Veracruz;
+    [SerializeField] private GameObject Yucatan;
+    [SerializeField] private GameObject Zacatecas;
+
+
     // Start is called before the first frame update
     void Start()
     {
