f5755299b14c4ec1a6059d8ff57a4ac5aafbf19c
TellezM <moshofut@gmail.com>
2024-04-12 15:02:37 -0600

    Avances

---
+++ EcosistemasMexicanos/Assets/Scripts/GameCont.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> btns = new List<Button>();
+    private Sprite bgImage;
+
+    void Start()
+    {
+        GetButtons();
+        AddListeners();
+    }
+    
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag ("PuzzleButton");
+
+        for (int i=0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = bgImage;
+        }
+    }
+    void AddListeners()
+    {
+        foreach(Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickAPuzzle());
+
+        }
+    }
+    public void PickAPuzzle()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+    }
+}
