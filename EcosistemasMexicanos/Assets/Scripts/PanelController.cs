654effbfe383b8fd779ca67c9b7691b42511b85a
marielsgtzz <marielsgtzz@gmail.com>
2024-04-13 12:10:10 -0600

    Mostrar paneles

---
+++ EcosistemasMexicanos/Assets/Scripts/PanelController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField] private GameObject firstPanel; // Panel that should be visible at the start.
+    [SerializeField] private GameObject secondPanel; // Additional panel to be toggled.
+    [SerializeField] private GameObject thirdPanel; // You can add as many as you need.
+
+    private void Start()
+    {
+        // Ensure that only the first panel is active when the game starts.
+        firstPanel.SetActive(true);
+        secondPanel.SetActive(false);
+        thirdPanel.SetActive(false);
+    }
+
+    // Call this method using a button's OnClick event to toggle the second panel.
+    public void ShowSecondPanel()
+    {
+        secondPanel.SetActive(true);
+    }
+
+    public void ShowThirdPanel()
+    {
+        thirdPanel.SetActive(true);
+    }
+
+
+    // If you want to toggle it off again with the same or a different button:
+    public void HidePanels()
+    {
+        secondPanel.SetActive(false);
+        thirdPanel.SetActive(false);
+    }
+
+
+}
