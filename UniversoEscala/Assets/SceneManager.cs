68484fb03f7b26db55632b7f41dfbc4d11b0f271
andressev <53363028+andressev@users.noreply.github.com>
2024-04-27 06:25:41 -0600

    Empieza a funcionar

---
+++ UniversoEscala/Assets/SceneManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [Header("Escenas")]
+    [SerializeField] private GameObject Escena1;
+    [SerializeField] private GameObject Escena2;
+    [SerializeField] private GameObject Escena3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Escena1.SetActive(true);
+        Escena2.SetActive(false);
+        Escena3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void ShowFirstPanel()
+    {
+        Escena1.SetActive(true);
+    }
+    public void ShowSecondPanel()
+    {
+        Escena2.SetActive(true);
+    }
+    //noton
+
+    public void ShowThirdPanel()
+    {
+        Escena3.SetActive(true);
+    }
+
+
+    // If you want to toggle it off again with the same or a different button:
+    public void HidePanels()
+    {
+        Escena1.SetActive(false);
+        Escena2.SetActive(false);
+        Escena3.SetActive(false);
+    }
+
+}
