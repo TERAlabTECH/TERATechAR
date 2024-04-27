dd4fc2374ea78834b7ac440c68798c5cd2d025d9
andressev <53363028+andressev@users.noreply.github.com>
2024-04-27 07:31:16 -0600

    drft inicial

--- UniversoEscala/Assets/Manager/GameManager.cs
+++ UniversoEscala/Assets/Manager/GameManager.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Escenas")] 
-    [SerializeField] private GameObject EarthVsMoon;
-    [SerializeField] private GameObject EarthVsSun;
-    [SerializeField] private GameObject SunVsProxima;
-    // Start is called before the first frame update
-
-   
+    
     
     void Start()
     {
@@ -18,15 +12,7 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
-    public void ShowScene(GameObject scene)
-    {   
-
-        scene.SetActive(false);
-    }
-
+    
 }
