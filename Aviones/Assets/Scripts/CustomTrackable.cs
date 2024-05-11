72950711191ea6ebcab9ce560a7e0a0536e0b6d4
marielsgtzz <marielsgtzz@gmail.com>
2024-05-11 09:44:17 -0600

    Encuentra Targets

---
+++ Aviones/Assets/Scripts/CustomTrackable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CustomTrackable : DefaultObserverEventHandler
+{
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        Debug.Log("Encontrado!");
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        Debug.Log("Perdido!");
+
+    }
+}
